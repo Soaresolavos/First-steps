fc45a1722f3a403ffccfc738c49a90e8f5632068
Olavo Soares <58673055+Soaresolavos@users.noreply.github.com>
2020-01-19 18:53:51 -0300

    commit

---
+++ Curso JS/aula54/aula54FucaoCallBack.js
@@ -0,0 +1,45 @@
+function rand(min = 1000, max = 3000) {
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num);
+}
+
+function f1(callback) {
+    setTimeout(function () {
+        console.log("f1");
+        if (callback) callback();
+    }, rand());
+}
+function f2(callback) {
+    setTimeout(function () {
+        console.log("f2");
+        if (callback) callback();
+    }, rand());
+}
+function f3(callback) {
+    setTimeout(function () {
+        console.log("f3");
+        if (callback) callback();
+    }, rand());
+}
+
+// f1(function(){
+//     f2(function(){
+//         f3(function(){
+//             console.log("Olá mundo");
+//         });
+//     });
+// });
+// Mesmo código de outra maneira mais clara abaixo
+f1(f1callback);
+function f1callback(){
+    f2(f2callback);
+}    
+
+function f2callback() {
+    f3(f3callback);
+}
+
+function f3callback() {
+    console.log('Olá mundo!');
+}
+
