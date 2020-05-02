40bd2eec0cdd8777e0ac7f3fd2ae894438a56df1
Olavo Soares <58673055+Soaresolavos@users.noreply.github.com>
2020-05-02 20:28:32 -0300

    commit

---
+++ Curso JS/aula91babelCanIuse/main.js
@@ -0,0 +1,5 @@
+console.log('Estou te vendo!');
+const nome = 'Olavo';
+const obj = {nome};
+const novoObj = {...obj};
+console.log(novoObj);
---
+++ Curso Web Development/Drum Kit/index.js
@@ -0,0 +1,45 @@
+
+const todosBotoes = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < todosBotoes; i++) {
+document.querySelectorAll(".drum")[i].addEventListener("click",  function () {
+   if (this.innerHTML === "w") {
+    let audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    }
+
+    if(this.innerHTML === "a") {
+        let audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+    }
+
+    if(this.innerHTML === "s") {
+        let audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+    }
+
+    if(this.innerHTML === "d") {
+        let audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+    }
+
+    if(this.innerHTML === "j") {
+        let audio = new Audio("sounds/crash.mp3");
+        audio.play();
+    }
+
+    if(this.innerHTML === "k") {
+        let audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+    }
+
+    if(this.innerHTML === "l") {
+        let audio = new Audio("sounds/snare.mp3");
+        audio.play();
+    }
+      
+});
+}
+
+// let audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
