1ec6329ead92c4b0a3e991ada750f016c149ff92
Olavo Soares <58673055+Soaresolavos@users.noreply.github.com>
2020-05-05 13:00:55 -0300

    commit

--- Curso Web Development/Drum Kit/index.js
+++ Curso Web Development/Drum Kit/index.js
@@ -1,44 +1,54 @@
 const todosBotoes = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < todosBotoes; i++) {
-document.querySelectorAll(".drum")[i].addEventListener("click",  function () {
-   if (this.innerHTML === "w") {
-    let audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-    }
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        let botaoInnerHtml = this.innerHTML;
+        makeSound(botaoInnerHtml);
 
-    if(this.innerHTML === "a") {
-        let audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-    }
-
-    if(this.innerHTML === "s") {
-        let audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-    }
-
-    if(this.innerHTML === "d") {
-        let audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-    }
-
-    if(this.innerHTML === "j") {
-        let audio = new Audio("sounds/crash.mp3");
-        audio.play();
-    }
-
-    if(this.innerHTML === "k") {
-        let audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
-    }
-
-    if(this.innerHTML === "l") {
-        let audio = new Audio("sounds/snare.mp3");
-        audio.play();
-    }
-      
-});
+    });
 }
 
-// let audio = new Audio("sounds/tom-1.mp3");
-// audio.play();
+document.addEventListener("keydown", function(event){
+   makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            let snare = new Audio("sounds/crash.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            let crash = new Audio("sounds/kick-bass.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            let kick = new Audio("sounds/snare.mp3");
+            kick.play();
+            break;
+
+        default: console.log(botaoInnerHtml)
+    }
+}
