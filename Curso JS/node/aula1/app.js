585d2a8e2fe1450d1099df41af7cc173985bf519
Olavo Soares <58673055+Soaresolavos@users.noreply.github.com>
2020-06-07 17:17:45 -0300

    commit

---
+++ Curso JS/node/aula1/app.js
@@ -0,0 +1,9 @@
+// const completeName = require('./mod1').completeName;
+// console.log(completeName());
+
+const Pessoa = require('./mod1').Pessoa;
+
+const p1 = new Pessoa('Olavo');
+const p2 = new Pessoa('André');
+console.log(p1);
+console.log(p2);
---
+++ Curso JS/node/aula1/mod1.js
@@ -0,0 +1,22 @@
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+
+exports.Pessoa = Pessoa;
+
+// const nome = 'Olavo';
+// const sobrenome = 'Conceição';
+
+// const falaNome = () => nome + ' ' + sobrenome;
+
+
+// module.exports.name = nome;
+// module.exports.lastName = sobrenome;
+// module.exports.completeName = falaNome;
+    
+    // exports.name = nome;
+    // exports.lastName = sobrenome;
+    // exports.completeName = falaNome;
+    // this.chave = "Dados";
